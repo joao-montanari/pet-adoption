858abf6b1d7373364b597dcf8f7bf63952bd1197
joao-montanari <joao.unipbr@gmail.com>
2025-05-22 10:42:10 -0300

    :sparkles: feat(frontend): Cria a página inicial

---
+++ frontend/src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import './style.sass';
+
+const HomePage = () => {
+    return (
+        <div className="master-container-home-page">
+            <h1>This is my home page</h1>
+        </div>
+    )
+}
+
+export default HomePage;
