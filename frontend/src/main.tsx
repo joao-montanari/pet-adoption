c4c3ca1813f4449ae68cd157354babd14a166374
joao-montanari <joao.unipbr@gmail.com>
2025-05-22 10:39:13 -0300

    :sparkles: feat(frontend): Adiciona e configura o react-router-dom

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { RouterProvider } from 'react-router-dom';
+
+import routers from './router';
 import './styles/main.sass';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <RouterProvider router={routers}/>
   </StrictMode>,
 )
---
+++ frontend/src/router.tsx
@@ -0,0 +1,5 @@
+import { createHashRouter } from "react-router-dom";
+
+const routers = createHashRouter([]);
+
+export default routers;
