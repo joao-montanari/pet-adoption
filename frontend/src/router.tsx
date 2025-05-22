2cbbb1bda47013014a866e80c97a8a710d949802
joao-montanari <joao.unipbr@gmail.com>
2025-05-22 10:43:04 -0300

    :construction: refactor(frontend): Adiciona as primeiras rotas do site

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,5 +1,20 @@
 import { createHashRouter } from "react-router-dom";
 
-const routers = createHashRouter([]);
+import Default from "@Layouts/Default";
+
+import HomePage from "@Pages/home";
+
+const routers = createHashRouter([
+    {
+        path: "",
+        element: <Default/>,
+        children: [
+            {
+                path: "/",
+                element: <HomePage/>,
+            }
+        ]
+    }
+]);
 
 export default routers;
